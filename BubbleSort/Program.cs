97fa1aa3f337f04eaa3b08ed1b15791f7a444320
Asravin <maisweedfell@gmail.com>
2024-02-27 09:37:12 +0200

    Пузырьковая сортировка

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+for(int i = 0; i < number; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Начальный массив: [" + string.Join(",", array) + "]");
+Console.WriteLine();
+
+for(int i = 0; i < number; i++)
+{
+    for(int j = 0; j < number - 1; j++)
+    {
+        if(array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+
+    Console.WriteLine(i + "[" + string.Join(",", array) + "]");
+}
