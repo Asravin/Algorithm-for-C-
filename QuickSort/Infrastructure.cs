6a31f53d50e809dd9799f4177feecf3f0fd5d686
Asravin <maisweedfell@gmail.com>
2024-03-13 13:40:26 +0200

    алгоритм быстрой сортировки

---
+++ QuickSort/Infrastructure.cs
@@ -0,0 +1,20 @@
+using static System.Console;
+using static System.String;
+
+public static class Infrastructure
+{
+ 
+  public static int[] CreateArray(this int size, int min = 0, int max = 10)
+  {
+    return Enumerable.Range(1, size)
+           .Select(item => Random.Shared.Next(min, max))
+           .ToArray();
+  }
+
+  public static int[] Show(this int[] array, string separator = ",")
+  {
+    string output = Join(separator, array);
+    WriteLine($"[{output}]");
+    return array;
+  }
+}
